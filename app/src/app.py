52147bdfdd245a0b2a1ed4c494d8507828c4f401
Oguzhan Ozdemir <aoozdemir@live.com>
2023-02-19 20:49:37 +0300

    Uncomment MySQL codes in the application

--- app/src/app.py
+++ app/src/app.py
@@ -1,35 +1,36 @@
 import os
 import flask
+import MySQLdb
 
 app = flask.Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def hello_world():
-  # storage = Storage()
-  # storage.populate()
-  # score = storage.score()
-  return "Hello Devops 123, %d!" % 10
+  storage = Storage()
+  storage.populate()
+  score = storage.score()
+  return "Hello Devops 123, %d!" % score
 
-# class Storage():
-#   def __init__(self):
-#     self.db = MySQLdb.connect(
-#       user   = os.getenv('MYSQL_USERNAME'),
-#       passwd = os.getenv('MYSQL_PASSWORD'),
-#       db     = os.getenv('MYSQL_INSTANCE_NAME'),
-#       host   = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
-#       port   = int(os.getenv('MYSQL_PORT_3306_TCP_PORT'))
-#     )
+class Storage():
+  def __init__(self):
+    self.db = MySQLdb.connect(
+      user   = os.getenv('MYSQL_USERNAME'),
+      passwd = os.getenv('MYSQL_PASSWORD'),
+      db     = os.getenv('MYSQL_INSTANCE_NAME'),
+      host   = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
+      port   = int(os.getenv('MYSQL_PORT_3306_TCP_PORT'))
+    )
 
-#     cur = self.db.cursor()
-#     cur.execute("CREATE TABLE IF NOT EXISTS scores(score INT)")
+    cur = self.db.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS scores(score INT)")
 
-#   def populate(self):
-#     cur = self.db.cursor()
-#     cur.execute("INSERT INTO scores(score) VALUES(1234)")
+  def populate(self):
+    cur = self.db.cursor()
+    cur.execute("INSERT INTO scores(score) VALUES(1234)")
 
-#   def score(self):
-#     cur = self.db.cursor()
-#     cur.execute("SELECT * FROM scores")
-#     row = cur.fetchone()
-#     return row[0]
+  def score(self):
+    cur = self.db.cursor()
+    cur.execute("SELECT * FROM scores")
+    row = cur.fetchone()
+    return row[0]
