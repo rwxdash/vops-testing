eb029e41aed6b765ee7dcb9425acddc203ac327c
Oguzhan Ozdemir <aoozdemir@live.com>
2023-02-19 17:01:28 +0300

    Configure a running gunicorn server for a simple hello-world endpoint

---
+++ app/src/app.py
@@ -0,0 +1,35 @@
+import os
+import flask
+
+application = flask.Flask(__name__)
+application.debug = True
+
+@application.route('/')
+def hello_world():
+  # storage = Storage()
+  # storage.populate()
+  # score = storage.score()
+  return "Hello Devops 123, %d!" % 10
+
+# class Storage():
+#   def __init__(self):
+#     self.db = MySQLdb.connect(
+#       user   = os.getenv('MYSQL_USERNAME'),
+#       passwd = os.getenv('MYSQL_PASSWORD'),
+#       db     = os.getenv('MYSQL_INSTANCE_NAME'),
+#       host   = os.getenv('MYSQL_PORT_3306_TCP_ADDR'),
+#       port   = int(os.getenv('MYSQL_PORT_3306_TCP_PORT'))
+#     )
+
+#     cur = self.db.cursor()
+#     cur.execute("CREATE TABLE IF NOT EXISTS scores(score INT)")
+
+#   def populate(self):
+#     cur = self.db.cursor()
+#     cur.execute("INSERT INTO scores(score) VALUES(1234)")
+
+#   def score(self):
+#     cur = self.db.cursor()
+#     cur.execute("SELECT * FROM scores")
+#     row = cur.fetchone()
+#     return row[0]
