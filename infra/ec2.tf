73f378e59dd9cb511d4287f7f108449b555b429e
Oguzhan Ozdemir <aoozdemir@live.com>
2023-02-20 23:41:26 +0300

    Open all connections to nodes

--- infra/ec2.tf
+++ infra/ec2.tf
@@ -3,18 +3,19 @@ resource "aws_security_group" "kubernetes_node_sg" {
   description = "Allow access from ports 6443"
 
   ingress {
-    description = "Access Port 6443 from Everywhere"
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 
   tags = {
@@ -51,6 +52,7 @@ resource "aws_instance" "k8s_worker_node_01" {
   iam_instance_profile        = aws_iam_instance_profile.k8s_node_role.name
   user_data_replace_on_change = false
   user_data                   = file("./scripts/worker-node-init.sh")
+  security_groups             = [aws_security_group.kubernetes_node_sg.name]
 
   depends_on = [
     aws_instance.k8s_master_node_01
